8ea0463559e3eb732d629c430f8f041a3bec6962
qaxim <compoxition@gmail.com>
2022-04-14 12:15:16 -0700

    1 file modified, 3 added

--- src/index.js
+++ src/index.js
@@ -15,8 +15,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="blogs" element={<Blogs />} />
-          <Route path="contact" element={<Contact />} />
+           <Route path="contact" element={<Contact />} />
           <Route path="*" element={<NoPage />} />
         </Route>
       </Routes>
